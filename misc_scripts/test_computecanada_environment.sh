ac87144fb4fb879ea4a547adf624faf78cd1206a
Lael Barlow <lael@ualberta.ca>
2019-06-10 18:19:17 -0600

    Modify script for testing python virtualenvironment on computecanada.

--- misc_scripts/test_computecanada_environment.sh
+++ misc_scripts/test_computecanada_environment.sh
@@ -24,19 +24,37 @@ module load iq-tree/1.5.5
 #module load mrbayes/3.2.6
 module load muscle/3.8.31
 
+# Load modules that are dependencies for ete3.
+module load qt/5.11.3
+
 # Make a virtual environment for python and install python modules.
 virtualenv --no-download $SLURM_TMPDIR/env
 source $SLURM_TMPDIR/env/bin/activate
 pip install --upgrade pip
 pip install biopython --no-index
-pip install ete3 --no-index
 pip install gffutils
 pip install PyPDF2 
 pip install reportlab
 
+# Install dependencies of ete3
+pip install six
+pip install PyQt5
+
+# Install ete3.
+pip install ete3 --no-index
+
+# Write a python script with an import statement. 
+PYTHONSCRIPT="test_imports.py"
+echo #/usr/bin/env python3 > $PYTHONSCRIPT
+echo from PyQt5 import QtGui, QtCore >> $PYTHONSCRIPT
+PYTHONOUTPUT="test_imports_output.txt"
+python3 $PYTHONSCRIPT > $PYTHONOUTPUT
 
 # Run amoebae commands to check whether all the subprocess calls and import statements will work.
-amoebae check_depend
-amoebae check_imports
+#amoebae check_depend
+#amoebae check_imports
+
+# Shut down the virtual environment.
+deactivate
 
 
