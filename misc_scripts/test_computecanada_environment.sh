b08918b9f7574ae625032c744c34b424c7f1243e
Lael Barlow <lael@ualberta.ca>
2019-06-11 08:12:47 -0600

    Modify script for testing python virtualenvironment on computecanada.

--- misc_scripts/test_computecanada_environment.sh
+++ misc_scripts/test_computecanada_environment.sh
@@ -11,7 +11,10 @@
 # repository) and run the script ( ./test_computecanada_environment.sh ).
 
 # Load python.
-module load python/3.7.0
+#module load python/3.7.0
+# Load older version so that it will work with PyQt
+module load python/3.6.3
+
 # Load this because it has pandas and numpy.
 module load scipy-stack 
 
@@ -25,7 +28,9 @@ module load iq-tree/1.5.5
 module load muscle/3.8.31
 
 # Load modules that are dependencies for ete3.
-module load qt/5.11.3
+#module load qt/5.11.3
+# Load an older version of qt which actually works (with python 3.6.3)
+module load qt/5.10.1
 
 # Make a virtual environment for python and install python modules.
 virtualenv --no-download $SLURM_TMPDIR/env
@@ -38,21 +43,22 @@ pip install reportlab
 
 # Install dependencies of ete3
 pip install six
-pip install PyQt5
 
 # Install ete3.
 pip install ete3 --no-index
 
 # Write a python script with an import statement. 
-PYTHONSCRIPT="test_imports.py"
+PYTHONSCRIPT="test_pyqt5_import.py"
 echo #/usr/bin/env python3 > $PYTHONSCRIPT
 echo from PyQt5 import QtGui, QtCore >> $PYTHONSCRIPT
-PYTHONOUTPUT="test_imports_output.txt"
+PYTHONOUTPUT="test_pyqt5_import_output.txt"
 python3 $PYTHONSCRIPT > $PYTHONOUTPUT
 
 # Run amoebae commands to check whether all the subprocess calls and import statements will work.
-#amoebae check_depend
-#amoebae check_imports
+printf "\n\n\nChecking dependencies of amoebae:\n"
+amoebae check_depend
+printf "\n\n\nChecking all import statements for amoebae:\n"
+amoebae check_imports
 
 # Shut down the virtual environment.
 deactivate
