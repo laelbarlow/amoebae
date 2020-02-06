f66996635752b074c6792d490a3e1cb76d3f2182
Lael Barlow <lael@ualberta.ca>
2020-02-06 09:40:12 -0700

    Update .gitignore file.

--- documentation/build_pdf.sh
+++ documentation/build_pdf.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-DOCTEX=AMOEBAE_documentation.tex
-pdflatex --shell-escape $DOCTEX &&\
+DOCTEX=AMOEBAE_documentation
+pdflatex --shell-escape $DOCTEX.tex &&\
 bibtex $DOCTEX &&\
 makeglossaries $DOCTEX &&\
-pdflatex --shell-escape $DOCTEX &&\
-pdflatex --shell-escape $DOCTEX &&\
+pdflatex --shell-escape $DOCTEX.tex &&\
+pdflatex --shell-escape $DOCTEX.tex &&\
 open AMOEBAE_documentation.pdf
