679d1818f53af90cd535ac09fd6fd9b908e23276
Lael Barlow <lael@ualberta.ca>
2020-04-07 21:47:19 -0600

    Update setup.sh to work with Singularity instead of Docker, and to set up vagrant on if MacOs is detected.

--- setup.sh
+++ setup.sh
@@ -40,62 +40,66 @@ chmod a+x misc_scripts/*.sh
 
 
 #######################################################
-# Check whether Docker is installed or not.
-printf "\nDetermining whether Docker needs to be installed or not.\n\n"
-
-if test "$(command -v docker)"; then
-    printf "\tDocker is installed.\n\n"
-    
-else
-    printf "\tDocker is not installed.\n\n"
-
-# If Docker is not installed, detect what operating system is being used, and
-# install Docker.
+# If Vagrant is not installed, detect what operating system is being used, and
+# install Vagrant if necessary.
 
 if [ "$(uname)" == "Darwin" ]; then
-printf "\nDetected MacOS."
-# Do something under Mac OS X platform
-# Prompt user to install Docker Desktop for MacOS manually.
-printf "\nInstall Docker Desktop for MacOS from the website:\n" &&\
-printf "\n\thttps://docs.docker.com/docker-for-mac/install/\n\n" &&\
-read -p "\nDone? (press enter to continue)" &&\
-# Prompt user to add the amoebae repo path to the files for sharing via the
-# Docker Desktop App GUI.
-printf "If you have not already done so, add this directory path to the Docker
-files that can be shared and mounted to Docker containers via the Docker
-graphical user interface:\n\n" &&\
-pwd &&\
-read -p "\nDone? (press enter to continue)" &&\
-# Prompt user to customize the resources provided to docker for running
-# containers via the Docker Desktop App GUI.
-printf "If you have not already done so, customize settings for defining number
-of CPUs and amount of memory, swap space, and storage made available to Docker
-containers that will run on your machine, via the Docker graphical user
-interface. These can be accessed in Preferences > Resources.\n\n" &&\
-read -p "\nDone? (press enter to continue)"
+    printf "\nDetected MacOS."
+    # Do something under Mac OS X platform
+
+    # Check whether Vagrant is installed or not.
+    printf "\nDetermining whether Vagrant needs to be installed or not.\n\n"
+
+    if test "$(command -v vagrant)"; then
+        printf "\tVagrant is installed.\n\n"
+        
+    else
+        printf "\tVagrant is not installed.\n\n"
+
+        # Install Virtualbox (and Homebrew, if necessary).
+        printf "\nInstalling dependencies of Vagrant."
+        if test "$(command -v virtualbox)"; then
+        if test "$(command -v brew)"; then
+        /bin/bash -c "$(curl -fsSL \
+        https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+        fi
+
+        brew cask install virtualbox
+
+        fi
+
+        # Install Vagrant on MacOS.
+        brew cask install vagrant && \
+        brew cask install vagrant-manager
+
+        # Proceed with building a .sif file.
+        printf "\tBuilding singularity container image file.\n\n"
+        /bin/bash singularity_build_on_mac.sh
+    fi
+
 
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     printf "\nDetected Linux."
     # Do something under GNU/Linux platform
 
-    # Install docker.
-    # This should work on ubuntu:
-    #sudo apt-get install -y \
-    #    apt-transport-https \
-    #    ca-certificates \
-    #    curl \
-    #    software-properties-common curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add - sudo add-apt-repository \
-    #    "deb https://apt.dockerproject.org/repo/ \
-    #    ubuntu-$(lsb_release -cs) \
-    #    main" sudo apt-get update
-    #sudo apt-get -y install docker-engine # add current user to docker group so there is no need to use sudo when running docker
-    #sudo usermod -aG docker $(whoami)
+    if test "$(command -v singularity)"; then
+        printf "\tSingularity is installed.\n\n"
 
-    printf "\nProcedures for installing Docker on linux vary. Consult the
-    Docker documentation: https://docs.docker.com/install/"
+        # Proceed with building a .sif file.
+        printf "\tBuilding singularity container image file.\n\n"
+        /bin/bash singularity_build.sh
+        
+    else
+        printf "\tSingularity is not installed.\n\n"
 
-    # ***Note: It may be necessary to customize memory and CPU usage options:
-    # https://docs.docker.com/config/containers/resource_constraints/
+        printf "\nProcedures for installing singularity on linux vary. If
+        singularity is not installed on your system, consult the singularity
+        documentation, install singularity, and re-run this script. This will
+        not work on clusters where you do not have administrative privileges.
+
+            https://sylabs.io/guides/3.0/user-guide/installation.html"
+        exit 1
+    fi
 
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
     printf "\nDetected Windows."
@@ -110,22 +114,6 @@ elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
     exit 1
 fi
 
-fi
-
-
-#######################################################
-# Build Docker image from Dockerfile.
-# This generates a Docker image and container using a Docker
-# file in the current directory which should be the amoebae git repository
-# directory. 
-
-# Change the owner of the files to be mounted to the container on your host machine.
-#echo Changing permissions for current directory ${PWD}/notebooks... &&\
-#sudo chown -R 1000:100 ${PWD}/notebooks &&\
-# Build a docker image using the Dockerfile file in the amoebae-lite repo.
-printf "\nBuilding Docker image from Dockerfile..." &&\
-DI=amoebaedockerimage:1.0
-docker build -t $DI .
 
 
 #######################################################
