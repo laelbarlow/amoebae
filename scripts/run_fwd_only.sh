0fb705d98560710233cca7afaab3ba5466233ec5
Lael Barlow <lael@ualberta.ca>
2020-11-11 12:48:53 -0700

    Specify versions of dependencies in conda environment definition file.

--- scripts/run_fwd_only.sh
+++ scripts/run_fwd_only.sh
@@ -7,7 +7,8 @@ source scripts/determine_snakemake_profile.sh
 source scripts/workflow_python_env_definition.sh
 
 # Run snakemake in python virtual environment.
-snakemake get_fwd_hit_seqs -j 100 --use-singularity --profile $snakemake_profile
+#snakemake get_fwd_hit_seqs -j 100 --use-singularity --profile $snakemake_profile
+snakemake get_fwd_hit_seqs -j 100 --use-conda --profile $snakemake_profile
 
 # Deactivate python virtual environment.
 source scripts/deactivate_python_env.sh
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,18 +1,4 @@
 #!/usr/bin/env bash
-# Copyright 2018 Lael D. Barlow
-# 
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# 
-#     http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# 
 
 # Script for setting up AMOEBAE to run in a singularity container after cloning
 # the git repository. This is a bash script so it does not depend on python3
@@ -22,21 +8,6 @@
 
 # Start timing this script.
 SECONDS=0
-
-
-#######################################################
-# Copy the example settings file.
-if [ ! -f "settings.py" ]; then
-    printf "\tWriting settings file.\n\n"
-    cp settings.py.example settings.py
-fi
-
-
-#######################################################
-# Make scripts executable.
-chmod a+x amoebae
-chmod a+x misc_scripts/*.py
-chmod a+x misc_scripts/*.sh
 
 
 #######################################################
@@ -206,15 +177,6 @@ elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
     exit 1
 fi
 
-
-
-#######################################################
-# Run test code using pytest. This runs all the tests defined in files in the
-# tests subdirectory.
-#printf "\nTesting AMOEBAE source code using PyTest..." &&\
-#docker run --rm -it \
-#    -v ${PWD}:/opt/amoebae \
-#    $DI /bin/bash -c "pytest"
 
 
 #######################################################
