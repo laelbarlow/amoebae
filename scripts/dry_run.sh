625736ea115345f14fc0ef451feb47ff07d6c49f
Lael Barlow <lael@ualberta.ca>
2020-11-11 12:25:18 -0700

    Modify snakemake commands to use conda instead of singularity.

--- scripts/dry_run.sh
+++ scripts/dry_run.sh
@@ -7,10 +7,11 @@ source scripts/determine_snakemake_profile.sh
 source scripts/workflow_python_env_definition.sh
 
 # Run snakemake in python virtual environment.
+#snakemake plot_workflow -j 100 --use-singularity --profile $snakemake_profile \
 python3 --version && \
 snakemake --version && \
 snakemake -n  --snakefile workflow/Snakefile && \
-snakemake plot_workflow -j 100 --use-singularity --profile $snakemake_profile \
+snakemake plot_workflow -j 100 --use-conda --profile $snakemake_profile \
 --snakefile workflow/Snakefile --verbose
 
 # Deactivate python virtual environment.
