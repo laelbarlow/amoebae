0cca77c249e3b7e7c0205d7c8496af71eb8656d1
Lael Barlow <lael@ualberta.ca>
2021-03-26 11:55:00 -0600

    Update mamba command for installing snakemake and other dependencies, so that the additional dependencies are installed with mamba instead of conda.

--- scripts/workflow_python_env_definition.sh
+++ scripts/workflow_python_env_definition.sh
@@ -73,17 +73,17 @@ if test "$(command -v conda)"; then
 
 
     # Create conda environment for snakemake with mamba.
-    mamba create -c conda-forge -c bioconda -n conda_env_amoebae_snakemake_workflow snakemake
+    mamba create -c conda-forge -c bioconda \
+        -n conda_env_amoebae_snakemake_workflow \
+            snakemake \
+            matplotlib \
+            numpy \
+            graphviz \
+            requests \
+            pulp \
 
-    # Activate new conda environment.
-    conda activate conda_env_amoebae_snakemake_workflow
-
-    # Install additional dependencies in new conda environment.
-    conda install matplotlib
-    conda install numpy
-    conda install graphviz
-    conda install requests
-    conda install pulp
+    echo Environment set up. Re-run to activate.
+    exit 1
 
   else
     # Otherwise, just activate the environment.
