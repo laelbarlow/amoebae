5d15e037ece9cd1846724d1587f440737fbd11c6
Lael Barlow <lael@ualberta.ca>
2022-01-05 14:46:22 -0700

    Remove unnecessary Makefile and associated scripts.

--- scripts/activate_python_env.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-printf "\nActivating Python environment. Enter 'deactivate' when finished.\n\n"
-
-# Activate python virtual environment.
-source scripts/workflow_python_env_definition.sh
-
