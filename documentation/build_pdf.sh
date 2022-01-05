a52d68df7422afed95b33c306add8e7eec80ebbe
Lael Barlow <lael@ualberta.ca>
2022-01-05 13:44:40 -0700

    Typeset command-line documentation.

--- documentation/build_pdf.sh
+++ documentation/build_pdf.sh
@@ -2,5 +2,6 @@
 DOCTEX=amoebae_commands
 pdflatex --shell-escape $DOCTEX.tex &&\
 bibtex $DOCTEX &&\
+makeglossaries $DOCTEX &&\
 pdflatex --shell-escape $DOCTEX.tex &&\
 pdflatex --shell-escape $DOCTEX.tex
